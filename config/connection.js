cb9872d3079304a8c60c65fe9cbc4782390f0058
Michael Bailey <michaeljaybailey@gmail.com>
2020-09-23 14:33:43 -0700

    changes to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,14 @@
 // Connect Node to MySQL.
 var mysql = require("mysql");
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
+
+connection = mysql.createConnection({
         host: "localhost",
         user: "root",
         password: "password",
         database: "burgers_db"
     });
-};
+
 
 connection.connect(function(err) {
     if (err) {
